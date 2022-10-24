c0eaec0b86fd5dd3eef285cd9ccfcdaa75f71ddb
Agustin <agu-zaballa@hotmail.com>
2022-10-24 20:45:19 -0300

    Primera entrega

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Mostramos un alert al usuario
-alert("¡Bienvenido a Juegos para Todos!\nA continuación le mostraremos los juegos disponibles:\n(Todos los juegos listados tienen impuestos según su lugar de residencia)")
+alert("¡Bienvenido a Juegos para Todos!\nA continuación le mostraremos los juegos disponibles:\n(Todos los juegos listados tienen impuestos según su país de residencia)")
 
 // Declaro las variables
 let hollow = 179.99
@@ -30,16 +30,16 @@ do {
     opcion = Number(prompt("Seleccione el juego que desea comprar:\n1 - Hollow Knight --- ARS$ 179,99 \n2 - BioShock Infinite --- ARS$ 395,99\n3 - Far Cry 3 --- ARS$ 999,00\n4 - Counter-Strike --- ARS$ 129,99\n\n0 - CERRAR MENÚ"))
     switch (opcion) {
         case 1:
-            alert("El precio total sumado los impuestos es de: "+Math.round(calculoImpuestos(1)) + " ARS$")
+            alert(`El precio total sumado los impuestos es de: ${Math.round(calculoImpuestos(1))} ARS$`)
             break
         case 2:
-            alert("El precio total sumado los impuestos es de: "+Math.round(calculoImpuestos(2)) + " ARS$")
+            alert(`El precio total sumado los impuestos es de: ${Math.round(calculoImpuestos(2))} ARS$`)
             break
         case 3:
-            alert("El precio total sumado los impuestos es de: "+Math.round(calculoImpuestos(3)) + " ARS$")
+            alert(`El precio total sumado los impuestos es de: ${Math.round(calculoImpuestos(3))} ARS$`)
             break
         case 4:
-            alert("El precio total sumado los impuestos es de: "+Math.round(calculoImpuestos(4)) + " ARS$")
+            alert(`El precio total sumado los impuestos es de: ${Math.round(calculoImpuestos(4))} ARS$`)
             break
         case 0:
             alert("Gracias por visitarnos, cerrando...")
