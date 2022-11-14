58c2d409d9b942230ffae06fa2df21c40d366adf
Agustin <agu-zaballa@hotmail.com>
2022-11-13 23:42:02 -0300

    Cambios para la tercera entrega

--- script.js
+++ script.js
@@ -1,77 +1,136 @@
-// Mostramos un alert al usuario
-alert("¡Bienvenido a Juegos para Todos!\n(Todos nuestros juegos poseen un impuesto que dependerá de su país de residencia)")
+// Declaro un array de objetos con todos mis productos
 
-let videojuegos = [
-    { id: 1, nombre: "Hollow Knight", precio: 179.99, genero: "plataformas" },
-    { id: 2, nombre: "BioShock Infinite", precio: 395.99, genero: "disparos en primera persona" },
-    { id: 3, nombre: "Far Cry 3", precio: 999.00, genero: "mundo abierto" },
-    { id: 4, nombre: "Counter-Strike", precio: 129.99, genero: "disparos en primera persona" },
-    { id: 5, nombre: "Grand Theft Auto V", precio: 699.99, genero: "mundo abierto" }
+let productos = [
+    { id: 1, nombre: "Hollow Knight", precio: 179.99, genero: "Plataformas | Historia", img: "./images/hollow_knight.jpeg" },
+    { id: 2, nombre: "BioShock Infinite", precio: 395.99, genero: "FPS | Historia", img: "./images/bioshock_infinite.jpg" },
+    { id: 3, nombre: "Far Cry 3", precio: 999.99, genero: "FPS | Mundo abierto", img: "./images/far_cry3.webp" },
+    { id: 4, nombre: "Counter-Strike:Source", precio: 129.99, genero: "FPS | Juego de disparos táctico", img: "./images/counter_strike_source.jpg" },
+    { id: 5, nombre: "Grand Theft Auto V", precio: 699.99, genero: "Mundo abierto | Historia", img: "./images/grand_theft_auto_v.jpeg" },
+    { id: 6, nombre: "Dead by Daylight", precio: 999.99, genero: "Horror | Supervivencia", img: "./images/dead_by_daylight.jpeg" },
+    { id: 7, nombre: "FIFA 23", precio: 8999.99, genero: "Deportes | Fútbol", img: "./images/fifa23.jpg" },
+    { id: 8, nombre: "Fallout 4", precio: 799.99, genero: "Mundo abierto | RPG", img: "./images/fallout4.jpg" },
+    { id: 9, nombre: "Dying Light 2", precio: 2999.99, genero: "Mundo abierto | Zombies", img: "./images/dying_light.jpeg" },
+    { id: 10, nombre: "God of War", precio: 4199.99, genero: "Aventura | Historia", img: "./images/god_of_war.jpeg" },
+    { id: 11, nombre: "Grounded", precio: 3199.99, genero: "Supervivencia", img: "./images/grounded.jpeg" },
+    { id: 12, nombre: "Age of Empires II", precio: 299.99, genero: "Estrategia", img: "./images/age_of_empires2.jpeg" }
 ]
 
-let precioAFiltrar = prompt("Los juegos menores al precio que ingrese apareceran a continuación:")
+let contenedorProductos = document.getElementById("contenedorProductos")
 
-const carrito = [];
+// Código antiguo sin la función de filtrado
 
-let videojuegosOfrecidos = `Estos son los videojuegos que tenemos disponibles:\n`
+/*contenedorProductos.innerHTML = ''
+for (const { id, nombre, precio, genero, img } of productos) {
+    let tarjetaProducto = document.createElement('div')
+    tarjetaProducto.className = 'producto'
+    tarjetaProducto.innerHTML = `
+    <img src=${img}>
+    <h3 class= "productoTarjeta">${nombre}</h3>
+    <h4 class= "productoTarjeta">${genero}</h4>
+    <h5 class= "productoTarjeta">$${precio}</h5>
+    <button class="boton" id=${id}>Agregar al carrito</button>
+    `
+    contenedorProductos.append(tarjetaProducto)
+}*/
 
+// Filtro los productos mediante un input
 
-function agregaCarrito() {
-    for (juego of videojuegos.filter((juego) => juego.precio < precioAFiltrar)) {
-        videojuegosOfrecidos += ` \n ${juego.id} - ${juego.nombre}  |  $ ${juego.precio} ARS`
+renderizarProductos()
+
+let inputBusqueda = document.getElementById('busqueda')
+
+inputBusqueda.oninput = () => {
+    let productosFiltrados = productos.filter(producto => producto.nombre.includes(inputBusqueda.value))
+    renderizarProductos(productosFiltrados)
+    actualizarCarrito()
+}
+
+function renderizarProductos(productosFiltrados) {
+    let productosARenderizar = productos
+    if (productosFiltrados) {
+        productosARenderizar = productosFiltrados
     }
-
-    videojuegosOfrecidos += `\n\nIngrese el número del videojuego que desea agregar al carrito. Para cerrar el carrito pulse 0 o ENTER`
-
-    let accionUsuario = Number(prompt(videojuegosOfrecidos))
-
-    while (isNaN(accionUsuario)) {
-        alert("Por favor, ingrese sólo numeros")
-        accionUsuario = Number(prompt(videojuegosOfrecidos))
+    contenedorProductos.innerHTML = ''
+    for (const { id, nombre, precio, genero, img } of productosARenderizar) {
+        let tarjetaProducto = document.createElement('div')
+        tarjetaProducto.className = 'producto'
+        tarjetaProducto.innerHTML = `
+    <img src=${img}>
+    <h3 class= "productoTarjeta">${nombre}</h3>
+    <h4 class= "productoTarjeta">${genero}</h4>
+    <h5 class= "productoTarjeta">$${precio}</h5>
+    <button class="boton" id=${id}>Agregar al carrito</button>
+    `
+        contenedorProductos.append(tarjetaProducto)
     }
+}
 
-    while (accionUsuario != 0) {
-        switch (accionUsuario) {
-            case 1:
-                carrito.push(videojuegos[0])
-                alert(`Agregamos a tu carrito el videojuego ${videojuegos[0].nombre}`)
-                break;
-            case 2:
-                carrito.push(videojuegos[1])
-                alert(`Agregamos a tu carrito el videojuego ${videojuegos[1].nombre}`)
-                break;
-            case 3:
-                carrito.push(videojuegos[2])
-                alert(`Agregamos a tu carrito el videojuego ${videojuegos[2].nombre}`)
-                break;
-            case 4:
-                carrito.push(videojuegos[3])
-                alert(`Agregamos a tu carrito el videojuego ${videojuegos[3].nombre}`)
-                break;
-            case 5:
-                carrito.push(videojuegos[4])
-                alert(`Agregamos a tu carrito el videojuego ${videojuegos[4].nombre}`)
-                break;
-            default:
-                alert("No tenemos ese videojuego")
-                break
+let botones = document.getElementsByClassName('boton')
+let carrito = document.getElementById('carrito')
+
+// Guardo el carrito en localStorage
+
+let carritoGuardado = JSON.parse(localStorage.getItem('carrito')) || []
+
+actualizarCarrito()
+renderizarCarrito()
+
+// Actualizo el carrito a medida que el usuario interactúa con los botones
+
+function actualizarCarrito() {
+    for (const boton of botones) {
+        boton.onclick = (e) => {
+            let productoBuscado = productos.find(productos => productos.id == e.target.id)
+
+            let posicionProductoEnCarrito = carritoGuardado.findIndex(productos => productos.id == productoBuscado.id)
+
+            if (posicionProductoEnCarrito != -1) {
+                carritoGuardado[posicionProductoEnCarrito].unidades++
+                carritoGuardado[posicionProductoEnCarrito].subtotal = carritoGuardado[posicionProductoEnCarrito].precioUnidad * carritoGuardado[posicionProductoEnCarrito].unidades
+            } else {
+                carritoGuardado.push({ id: productoBuscado.id, nombre: productoBuscado.nombre, precioUnidad: productoBuscado.precio, unidades: 1, subtotal: productoBuscado.precio })
+            }
+
+            localStorage.setItem('carrito', JSON.stringify(carritoGuardado))
+            renderizarCarrito()
         }
-        accionUsuario = Number(prompt(videojuegosOfrecidos))
     }
-    alert("Estos son los videojuegos que a elegido para comprar:")
-    mostrarCarrito();
 }
 
-let videojuegosCarrito = "Videojuegos seleccionados: \n"
-let precioCarrito = 0
-let precioConImpuestos = 0
+// Renderizo el carrito según si había o no un carrito previo
 
-function mostrarCarrito() {
-    for (videojuegoCarrito of carrito) {
-        videojuegosCarrito += ` \n - ${videojuegoCarrito.nombre}`
-        precioCarrito += videojuegoCarrito.precio
+function renderizarCarrito() {
+    carrito.innerHTML = `
+    <div class="itemCarrito">
+        <p>Videojuego:</p>
+        <p>Precio:</p>
+        <p>Cantidad:</p>
+        <p>Subtotal:</p>
+    </div>
+    `
+    let total = 0
+    for (const item of carritoGuardado) {
+        total += item.subtotal
+        carrito.innerHTML += `
+        <div class="itemCarrito">
+            <p>${item.nombre}</p>
+            <p>${item.precioUnidad}</p>
+            <p>${item.unidades}</p>
+            <p>$${Math.round(item.subtotal)}</p>
+        </div>
+        `
     }
-    precioConImpuestos += precioCarrito * 1.75
-    alert(`${videojuegosCarrito} \n\nPor un total de: $ ${Math.round(precioCarrito)} ARS\nTotal más los impuestos de Argentina: $ ${Math.round(precioConImpuestos)} ARS`)
+    carrito.innerHTML += `
+    <div class="itemCarrito">
+        <h3>Total: $${Math.round(total)}</h3>
+    </div>
+    `
 }
-agregaCarrito()
+
+// Botón para borrar LocalStorage y el carrito en si
+
+let btnBorrar = document.getElementById("btnBorrar")
+btnBorrar.onclick = () => {
+    localStorage.clear()
+    location.reload()
+}
